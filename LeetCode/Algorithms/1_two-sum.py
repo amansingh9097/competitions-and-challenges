d95cbb5171d223b1cce0b4e782bcd947ef09cb56
amansingh9097 <amansingh9097@gmail.com>
2020-06-20 14:54:01 +0530

    1 & 2

---
+++ LeetCode/Algorithms/1_two-sum.py
@@ -0,0 +1,21 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
+from itertools import combinations
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for pair in combinations(nums, 2):
+            if pair[0]+pair[1]==target:
+                if pair[0] == pair[1]:
+                    return nums.index(pair[0]), nums.index(pair[1], nums.index(pair[0])+1)
+                else:
+                    return nums.index(pair[0]), nums.index(pair[1])
---
+++ LeetCode/Algorithms/2_add_two_numbers.py
@@ -0,0 +1,35 @@
+"""
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        ans = ListNode()
+        l3 = ans
+        while (l1 or l2):
+            Sum = l1.val + l2.val + carry
+            carry = Sum // 10
+            l3.next = ListNode(Sum % 10)
+            l3 = l3.next
+            l2 = (l2.next if l2 is not None else None)
+            l1 = (l1.next if l1 is not None else None)
+            
+        if carry > 0:
+            l3.next = ListNode(carry)
+            
+        return ans.next
